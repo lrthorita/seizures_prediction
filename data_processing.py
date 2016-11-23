89bc50b516293e4f249a52c99de4d7f0cdf3e8ed
Luiz Ricardo Takeshi Horita <lrthorita@outlook.com>
2016-11-23 20:41:50 -0200

    Create get_dataset(mode) function. Load datas and convert them to feature space.

--- data_processing.py
+++ data_processing.py
@@ -15,6 +15,8 @@ IEEG_SAMPLING_RATE = 400
 SAMPLING_PERIOD = 1./400.
 N_SAMPLES_SEGMENT = 240000
 N_TRAINING_SAMPLES = 6042
+N_TEST_SAMPLES = 6126
+N_NEW_TEST_SAMPLES = 1908
 
 def load_matdata(filename):
     """Take the mat file and convert it in a numpy matrix variable with the 
@@ -22,6 +24,59 @@ def load_matdata(filename):
     matdata = scipy.io.loadmat(filename)
     data = matdata['dataStruct']['data'][0,0]
     return data
+
+
+def get_dataset(mode):
+    """Take all the training or test dataset and concatenate in 16 matrices in the
+    feature space (FFT + Filter(0~30Hz))."""
+    datas = np.loadtxt('../Seizures_Dataset/train_and_test_data_labels_safe.csv',
+                       dtype='str', delimiter=',', skiprows=1, usecols=(0,1))
+    
+    if mode == 'train':
+        N_SAMPLES = N_TRAINING_SAMPLES
+        datas = datas[0:N_SAMPLES,:] # Eliminate the test datas
+        directory = "../Seizures_Dataset/train_"
+    elif mode == 'test':
+        N_SAMPLES = np.size(datas,0) - N_TRAINING_SAMPLES
+        datas = datas[N_TRAINING_SAMPLES:np.size(datas,0),:] # Eliminate the test datas
+        directory = "../Seizures_Dataset/test_"
+    
+    
+    feature_dataset = {}
+    corrupted_list = []
+    for c in xrange(16):
+        key = "channel_%s" % (c+1)
+        feature_dataset[key] = np.zeros((13201,N_SAMPLES))
+    for i in xrange(N_SAMPLES):
+        if corrupted_list.count(i) == 0:
+            # Load the i-th sample
+            file_dir = directory + "%s/" % datas[i,0][0]
+            file_path = file_dir + datas[i,0]
+            try:
+                sample = load_matdata(file_path)
+                # Get spectrum in the range 8-30 Hz
+                fft_feature = get_feature(sample)
+                feature = abs(fft_feature['energy'])/N_SAMPLES_SEGMENT
+
+                for c in xrange(16):
+                    key = "channel_%s" % (c+1)
+                    feature_dataset[key][:,i] = feature[:,c]
+                
+                print str(i) + "-th feature added."
+                    
+            except KeyboardInterrupt:
+                print "Interrupted!"
+                break
+            except:
+                print "The file " + datas[i,0] + " is corrupted."
+                corrupted_list.append(i)
+                continue
+    
+    # Save the cross-correlation matrices
+    filename = "feature_" + mode + "_dataset.pkl"
+    joblib.dump(feature_dataset,filename)
+    
+    return feature_dataset
 
 
 def get_fft(data):
@@ -41,7 +96,8 @@ def get_and_filter_fft(data):
 
     for i in xrange(len(frequencies)):
         freq = frequencies[i]
-        if (freq < -30) or ((freq > -8) and (freq < 8)) or (freq > 30):
+#        if (freq < -30) or ((freq > -8) and (freq < 8)) or (freq > 30):
+        if (freq < -30) or (freq > 30):
             fft_data[i] = 0
     
     return fft_data, frequencies
@@ -51,6 +107,15 @@ def get_feature(data):
     """Get the frequency range where alpha, beta and mu waves are situated.
     These waves are the waves that can, possibly, advise a future seizure 
     event. The frequency range is between 8 Hz and 30 Hz."""
+#    fft_data, frequencies = get_fft(data)
+#    fft_feature = np.zeros((31,16))
+#    for i in xrange(31):
+#        # Sample spectrum for integer values of frequency from 0 to 30 Hz
+#        # The index of i Hz
+#        i_index = np.int(i * N_SAMPLES_SEGMENT / IEEG_SAMPLING_RATE)
+#        fft_feature[i,:] = abs(fft_data[i_index,:])/N_SAMPLES_SEGMENT
+#
+#    return fft_feature
     lower_index = np.int(8 * N_SAMPLES_SEGMENT / IEEG_SAMPLING_RATE)
     higher_index = np.int(30 * N_SAMPLES_SEGMENT / IEEG_SAMPLING_RATE) + 1
 
@@ -97,7 +162,7 @@ def fft_train_cross_correlation():
                 sample_i = load_matdata(file_path)
                 # Get spectrum in the range 8-30 Hz
                 fft_feature_i = get_feature(sample_i)
-                feature_i = abs(fft_feature_i['energy'])/N_SAMPLES_SEGMENT
+                feature_i = fft_feature_i['energy'].real/N_SAMPLES_SEGMENT
             except KeyboardInterrupt:
                 print "Interrupted!"
                 break
@@ -118,8 +183,7 @@ def fft_train_cross_correlation():
                             sample_j = load_matdata(file_path)
                             # Get spectrum in the range 8-30 Hz
                             fft_feature_j = get_feature(sample_j)
-                            feature_j = abs(fft_feature_j['energy'])/N_SAMPLES_SEGMENT
-                            
+                            feature_j = fft_feature_j['energy'].real/N_SAMPLES_SEGMENT
                             for c in xrange(16):
                                 key = "channel_%s" % (c+1)
                                 # Correlation between the i-th sample and the j-th sample
@@ -157,9 +221,9 @@ def train_cross_correlation():
     corrupted_list = []
     for c in xrange(16):
         key = "channel_%s" % (c+1)
-        train_xcorr[key] = np.zeros((N_TRAINING_SAMPLES,N_TRAINING_SAMPLES))
+        train_xcorr[key] = np.zeros((3,3))
     
-    for i in xrange(N_TRAINING_SAMPLES):
+    for i in xrange(3):
         if corrupted_list.count(i) == 0:
             # File not corrupted
             corrupted_i = False
@@ -183,7 +247,7 @@ def train_cross_correlation():
                 continue
             
             if corrupted_i == False: # Check if the i-th file is corrupted
-                for j in xrange(i,N_TRAINING_SAMPLES):
+                for j in xrange(i,3):
                     if corrupted_list.count(j) == 0:
                         # Load the j-th sample
                         file_dir = "../Seizures_Dataset/train_%s/" % datas[j,0][0]
@@ -214,7 +278,7 @@ def train_cross_correlation():
                             continue
             
     # Save the cross-correlation matrices
-    joblib.dump(train_xcorr,'fft_train_xcorr.pkl')
+    joblib.dump(train_xcorr,'train_xcorr.pkl')
     
     return train_xcorr
 
@@ -222,6 +286,7 @@ def train_cross_correlation():
 
 
 # ================================ MAIN ================================== #
-fft_train_xcorr = train_cross_correlation()
+#fft_train_xcorr = fft_train_cross_correlation()
+#train_xcorr = train_cross_correlation()
 
-
+feature_dataset = get_dataset('train')
